afecb82bf15ba689e542e424fd8ad0c2210c2aa0
Unknown <przemek.rakowski@gmail.com>
2018-09-15 00:37:04 +0200

    map component

---
+++ letslearn/src/components/Map/config.js
@@ -0,0 +1,16 @@
+
+export const wrapperStyles = {
+    width: "100%",
+    maxWidth: 980,
+    margin: "0 auto",
+}
+
+export const cities = [
+    { name: "Zurich", coordinates: [8.5417, 47.3769] },
+    { name: "Singapore", coordinates: [103.8198, 1.3521] },
+    { name: "San Francisco", coordinates: [-122.4194, 37.7749] },
+    { name: "Sydney", coordinates: [151.2093, -33.8688] },
+    { name: "Lagos", coordinates: [3.3792, 6.5244] },
+    { name: "Buenos Aires", coordinates: [-58.3816, -34.6037] },
+    { name: "Shanghai", coordinates: [121.4737, 31.2304] },
+]
